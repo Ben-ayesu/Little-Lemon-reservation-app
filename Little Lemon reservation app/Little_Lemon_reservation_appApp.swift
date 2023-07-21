d20752487dfefb199d4010e054f50016b82de1d6
benjaminayesu-attah <ben.ayesu.attah@gmail.com>
2023-07-21 10:21:40 -0700

    Initial Commit

---
+++ Little Lemon reservation app/Little_Lemon_reservation_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_reservation_appApp.swift
+//  Little Lemon reservation app
+//
+//  Created by Benjamin Ayesu-Attah on 2023-07-21.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_reservation_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
